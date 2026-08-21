8764b0a2c9714a940d187406d0b48c09ff176ffc
Melchizedek Ekiru Ebenyo <ebenyomelchizedek@gmail.com>
2026-08-21 21:50:36 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,5 @@
-from utils import square, is_even, celsius_to_fahrenheit
-
-number = float(input("Enter a number: "))
-
-print("Square:", square(number))
-
-if is_even(number):
-    print("The number is even.")
-else:
-    print("The number is odd.")
-
-print("Fahrenheit:", celsius_to_fahrenheit(number))
+from utils import greet
 
 name = input("Enter your name: ")
+
 print(greet(name))
